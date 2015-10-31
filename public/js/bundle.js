c8436a1566777edc7bdcec9fbce1bd72e29484a2
Jacob <jacob.pagano@gmail.com>
2015-10-31 18:37:02 -0500

    testing

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -18988,7 +18988,7 @@ var ReactDOM = require('react-dom');
 ReactDOM.render(React.createElement(
     'h1',
     null,
-    'Hi Faygoat'
+    'I <3 Mercedes :) '
 ), document.getElementById("content"));
 
 },{"react":157,"react-dom":2}]},{},[158]);
